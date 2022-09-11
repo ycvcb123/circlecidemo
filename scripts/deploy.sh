d3d8501d48e600460648f08f34e77b9cac246b71
winsonye <winsonye@tencent.com>
2022-09-11 12:57:08 +0800

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,14 +24,14 @@ echo 'name is: '$GH_NAME
 echo 'siteSource is: '$siteSource
 
 
-# 切换到gh-pages分支
-if git rev-parse --verify origin/gh-pages >/dev/null 2>&1; then
-    git checkout origin/gh-pages
+# 切换到gh-pagess分支
+if git rev-parse --verify origin/gh-pagess >/dev/null 2>&1; then
+    git checkout origin/gh-pagess
     # 删除旧的文件内容
     git rm -rf .
 else 
     # https://im.shellj.com/2020/04/clean-git-history.html (orphan)
-    git checkout --orphan gh-pages
+    git checkout --orphan gh-pagess
 fi
 
 # 把构建好的文件目录给拷贝进来
@@ -46,11 +46,11 @@ git add -A
 git commit --allow-empty -m "deploy to github pages [ci skip]"
 
 # 推送远程文件
-git push --force --quiet origin  gh-pages
+git push --force --quiet origin  gh-pagess
 
 # 资源回收，删除临时的脚本和目录
 cd ..
 rm -rf gh-pages-branch
 
-echo "Finished Deployment!！！！"
+echo "Finished Deployment!!!!!!!"
 
